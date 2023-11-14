3659c416f9e25b1f07adef2c4dd4a8f0981439ee
Miguelkzk <141929749+Miguelkzk@users.noreply.github.com>
2023-11-14 18:57:41 -0300

    Update TablaProductos.tsx

--- src/components/Productos/TablaProductos.tsx
+++ src/components/Productos/TablaProductos.tsx
@@ -4,15 +4,9 @@ import { ProductoService } from "../../services/ProductService";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import VerProducto from "./VerProducto";
-<<<<<<< HEAD
-import { Form } from "react-router-dom";
-import { FormControl } from "react-bootstrap";
 
-
-=======
 import { FormControl, Navbar } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
->>>>>>> Miguel
 interface PropsProducto {
     prodId: number;
 }
@@ -139,9 +133,3 @@ function TablaProductos() {
     </>);
 }
 export default TablaProductos;
-<<<<<<< HEAD
-=======
-/*
-className="col-xxl-2 col-xl-3 col-md-4 col-sm-5 col-9"
-*/
->>>>>>> Miguel
