a0e29bf7e13540bfc302e0f33c63f6238e312aa0
JuanmaVD <JuanmaVD21@gmail.com>
2023-11-15 17:30:29 -0300

    1

---
+++ src/pages/UnidadMedida.tsx
@@ -0,0 +1,12 @@
+import UnidadMedidaTabla from "../components/UnidadMedidaTabla/UnidadMedidaTabla";
+
+
+
+const Categorias = () => {
+    return (
+        <>
+            <UnidadMedidaTabla />
+        </>
+    )
+}
+export default Categorias;
